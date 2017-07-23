4c85542f7ef1276983b69f121c79d83afe2a391c
godblessearth <305312538@qq.com>
2017-07-23 16:11:06 +0800

    Add files via upload

---
+++ ACG/webs/load_regis/loadWeb.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<!-- 登陆页面 -->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="../../css/reset.css">
+<link rel="stylesheet" type="text/css" href="../../css/loadWeb.css">
+<link rel="stylesheet" type="text/css" href="../../css/doorwebsite.css">
+<title>ACG-funs 登陆</title>
+</head>
+<body>
+<!--顶部导航-->
+<div id="loadMenu">
+	<div class="logo"><img src="../../pictures/1.1.1.png" width="100" height="100"></div>
+	<div class="menulist">
+		<span class="menulistNames">
+				<a href="../index/index.php" target="_blank">主站</a>&nbsp|
+				<a href="loadWeb.php">登陆</a>&nbsp|
+				<a href="registerWeb.php">注册</a>&nbsp|
+		</span>
+	</div>
+</div>
+<div id="container">
+	<div id="pictures"><img class="picImg" src="../../pictures/1.5.jpg" width="100%" height="100%"></div>
+	<div id="load">
+		<div class="loadLogo"><h1>登陆</h1></div>
+		<hr/>
+		<form action="loadmatch.php" method="post" autocomplete="on">
+		<div class="loadInput"><input type="text" placeholder=" 请输入帐号"  name="fname"/></div>
+		<div class="loadInput"><input type="password" placeholder=" 您的密码" name="passwd"/></div>
+		<div class="frogetPass"><a href="loadWeb.html">忘记密码？></a></div>
+		<div class="btnLoad"><input class="loading" type="submit" value="登陆">
+		</form>
+			<a class="reg" href="registerWeb.php">注册</a>
+		</div>
+	</div>
+</div>
+
+<!--底层-->
+<div class="links">
+	<div class="links_left">
+		<p class="lintitle">友情链接</p>
+		<hr>
+		<p>
+			<a href="http://www.baidu.com" target="_blank">百度</a>&nbsp 
+			<a href="http://www.baidu.com" target="_blank">漫画之家</a>&nbsp  
+			<a href="http://www.baidu.com" target="_blank">哔哩哔哩</a>&nbsp
+			<a href="http://www.baidu.com" target="_blank">ACfun</a></p></br>
+		<p>
+			<a href="http://www.baidu.com" target="_blank">搜狐</a>&nbsp
+			<a href="http://www.baidu.com" target="_blank">腾讯动漫</a>&nbsp
+			<a href="http://www.baidu.com" target="_blank">土豆</a>&nbsp
+			<a href="http://www.baidu.com" target="_blank">优酷视频</a></p>
+	</div>
+	<div class="links_center">
+		<ul>
+			<li><a href="http://www.baidu.com" target="_blank">关于我们</a></li>
+			<li><a href="http://www.baidu.com" target="_blank">联系我们</a></li>
+			<li><a href="http://www.baidu.com" target="_blank">客服解答</a></li>
+			<li><a href="http://www.baidu.com" target="_blank">广告赞助</a></li>
+		
+		</ul>
+		<ul>
+			<li><a href="http://www.baidu.com" target="_blank">加入我们</a></li>
+			<li><a href="http://www.baidu.com" target="_blank">版权申明</a></li>
+			<li><a href="http://www.baidu.com" target="_blank">关于我们</a></li>
+			<li><a href="http://www.baidu.com" target="_blank">关于我们</a></li>
+		
+		</ul>
+	</div>
+	<div class="links_right">
+		<p>
+			互联网ICP备案：
+		</p>
+		<p>
+			免责申明：本站资源全部来自互联网，本站只做汇总收集工作。
+		</p>
+		<p>
+			官方微信
+		</p>
+	</div>
+</div>
+</body>
+</html>
